f137696b86aec5966c73813a35800fc4b61ee860
Angelo Ottendorfer <101193795+AngeloOttendorfer@users.noreply.github.com>
2023-01-08 09:48:21 +0100

    Update MonitorController.java

--- src/main/java/at/fhtw/bic/monitor/controller/MonitorController.java
+++ src/main/java/at/fhtw/bic/monitor/controller/MonitorController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MonitorController {
     private String message = "";
+    
+    @GetMapping("/api/message/reset")
+    public String resetMessage(){
+        this.message = "Everything works as expected";
+        return message;
+    }
         
     @GetMapping("api/message/set")
     @ResponseBody
