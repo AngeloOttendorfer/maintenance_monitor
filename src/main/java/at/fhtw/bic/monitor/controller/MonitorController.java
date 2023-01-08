850228454d1f656056947a2a5ac45b48be332799
Angelo Ottendorfer <ic21b113@technikum-wien.at>
2023-01-08 09:21:27 +0100

    update

--- src/main/java/at/fhtw/bic/monitor/controller/MonitorController.java
+++ src/main/java/at/fhtw/bic/monitor/controller/MonitorController.java
@@ -1,15 +1,22 @@
 package at.fhtw.bic.monitor.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MonitorController {
     private String message = "";
 
-    @GetMapping("/api/message/reset")
-    public String resetMessage(){
-        this.message = "Everything works as expected";
-        return message;
+    @GetMapping("api/message/set")
+    @ResponseBody
+    public String setMessage(@RequestParam String message){
+        this.message = message;
+
+        if(this.message.equals(message)){
+            return "ok";
+        }
+        return "not ok";
     }
 }
